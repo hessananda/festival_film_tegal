93dd511c12f52973a29a17346dd6a88f19cfa131
hesa <hessananda@gmail.com>
2017-06-09 17:30:48 +0700

    init proyek

---
+++ index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Festival Film Tegal</title>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/font-awesome.css" rel="stylesheet">
+<link href="css/main.css?ver=1.8" rel="stylesheet">
+<meta property="og:url" content="http://clcpurbalingga.id/festival/" />
+<meta property="og:type" content="article" />
+<meta property="og:title" content="Festival Film Tegal" />
+<meta property="og:description" content="8 Juli - 5 Agustus 2017" />
+<meta property="og:image" content="http://clcpurbalingga.id/festival/img/Landingpage-FFP2017.jpg" />
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:site" content="@clc_purbalingga" />
+<meta name="twitter:creator" content="@clc_purbalingga" />
+<meta property="og:url" content="http://clcpurbalingga.id/festival/" />
+<meta property="og:title" content="Festival Film Purbalingga" />
+<meta property="og:description" content="8 Juli - 5 Agustus 2017" />
+<meta property="og:image" content="http://clcpurbalingga.id/festival/img/Landingpage-FFP2017.jpg" />
+  </head>
+
+  <body style="background-color: #f7f8fc;">
+  <div id="wrapper">        
+  <div class="container">
+  <div class="content">
+  
+  <div class="row">
+
+        <div class="row">
+          
+          <div class="col-sm-12 col-xs-12 text-center">  
+          <center>
+          <div>
+          <img src="img/sponsor.jpg" class="responsive">
+          </div>
+          <br>          
+          <center>
+          <div>
+          <img src="img/BANNER.jpg" class="responsive">
+          </div>
+          <br><br>
+          <p style="color: #23488c;font-weight: 700;font-family: 'Roboto', sans-serif; font-size: 12px;">Didukung Oleh:</p>
+          <div>
+          <img src="img/sponsor2.jpg" class="responsive">
+          </div>
+          </center>
+          </center>
+          </div>
+        </div>
+        <br><br>
+        <div class="row">
+          <div class="col-sm-4 col-xs-12">
+                <div>
+                <img src="img/fft.jpg" class="responsive">
+                </div>
+                <br>
+                <span style="padding-left: 50px;color: #23488c;font-weight: 900">
+                  FORUM FILM TEGAL
+                </span>
+                  <p style="padding-left: 50px;" class="par">
+                  <br>
+                  Jl. Sultan Agung No. 222 RT 06/22
+                  <br>
+                  Kejambon, Tegal
+                  <br>
+                  Jawa Tengah 52124
+                  <br>
+                  085642616735 - Zulkarnain A.
+                  <br>
+                  forumfilmtegal@gmail.com                  
+                </p>
+                <p style="padding-left: 50px;">
+                <a href="https://www.facebook.com/groups/1553490401534964/" style="color: #4e68a5" ><i class="fa fa-facebook-square fa-3x"></i></a>
+                &nbsp;&nbsp;
+                <a href="https://www.instagram.com/festivalfilmtegal/" style="color: #407199"><i class="fa fa-instagram fa-3x"></i></a>
+                </p>
+
+          </div>          
+
+          <div class="col-sm-6 col-xs-12">
+                <p class="par">         
+                  Tegal sesungguhnya bukanlah daerah yang asing dengan sinema. Memiliki sejarak panjang bioskop; setidaknya tercatat 14 bioskop pernah beroperasi di Tegal sebelum runtuh satu-persatu dipenghujung tahun 90                                    
+                </p>
+                <p class="par">
+                  Tegal juga melahirkan dua sutradara besar Indonesia; Chaerul Umam dan Imam Tantowi. Baik Chaerul maupun Imam, melahirkan banyak judul film yang melegenda serta menorekhan sejarah di perfilman Indonesia.
+                </p>
+                <p class="par">
+                  Namun secara perlahan Tegal tercabut dari budaya sinema. Kurangnya ruang tonton serta ketiadaan aktivitas perfilman, membuat Tegal seolah hilang dari peta sinema Indonesia.
+                </p>
+                <p class="par">
+                  FESTIVAL FILM TEGAL 2017
+                </p>
+                <p class="par">
+                  Untuk kali pertama Festival Film Tegal 2017 (FFT 2017) diselengarakan. Merupakan usaha untuk kembali menghubungkan masyarakat Tegal dengan sejarah dan budaya sinemanya; mengaktualisasikan dengan kondisi kekinian. Membaca Tegal sebagai bagian dari sejarah sinema Indonesia sekaligus menggali potensi Tegal dari berbagai aspek.                  
+                </p>
+                <p class="par">
+                  FFT 2017 akan mengali dan melibatkan banyak aspek serta sektor, utamanya sektor pariwisata. Bagaimana festival film bisa berperan sebagai faktor penting dalam pengembangan ekonomi kreatif skala kecil-menengah dan memberikan kontribusi ekonomi.
+                </p>
+                <p class="par">
+                  Festival ini juga akan menumbuhkan potensi anak muda Tegal dan sekitarnya melalui berbagai program aktivitas sinema; dari workshop hingga kompetisi. Bagaimana anak-anak muda Tegal dan sekitarnya bisa tumbuh kembang dengan budaya sinema baru dan menghubungkannya dengan sejarah.
+                </p>
+          </div>          
+          <div class="col-sm-2 col-xs-12">
+            
+          </div>
+        </div>
+
+          <!-- <div> -->
+          <!-- <h4 style="color: #454543;letter-spacing: 3px;"><b>8 JULI - 5 AGUSTUS 2017</b></h4> -->
+          <!-- <h5 style="color: #454543;letter-spacing: 3px;font-size: 0.9em;line-height:1.3em;"><strong>PURBALINGGA | CILACAP | BANJARNEGARA<br>BANYUMAS | KEBUMEN</strong></h5> -->
+          
+          <!-- </div> -->
+          <!-- <br> -->
+          <!-- <div>
+          <p style="color: #454543;font-family: 'Roboto', sans-serif; font-size: 20px;">
+          <a type="button" class="btn btn-danger" href="https://www.youtube.com/watch?v=VSnZwgqbIV4&list=PLlBHzaZi94CkhUfDMNj2Cv47s9s8g_lyY"><span class="naik"><strong>Tonton vlog kami</strong></span> &nbsp;&nbsp;&nbsp; <i class="fa fa-youtube fa-2x"></i></a>
+          </p>
+          </div>
+          <br><br>
+          <img src="img/foot.png" class="responsive"> -->
+          
+          
+        
+
+  </div>
+
+  </div>
+  </div>
+
+
+  </div>
+
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.js"></script>
+  </body>
+</html>
